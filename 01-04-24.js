8e05a6a2677dffdf59bdd52cab116eacb2474471
Jennifer Sieber <jenwebdev2@gmail.com>
2024-01-04 14:22:11 -0700

    add jan 4 katas

---
+++ 01-04-24.js
@@ -0,0 +1,19 @@
+console.log('Jan 4, 2024')
+
+// kata 1 array
+const findNeedle = haystack => 'found the needle at position ' + haystack.indexOf('needle');
+
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "randomJunk"]));
+
+// kata 2 
+const positiveSum = arr => arr.filter(num => num > 0).reduce((ttl, cv) => ttl + cv, 0)
+console.log(positiveSum([1,-4,7,12]))
+
+// kata 3 //3453455
+const sumTwoSmallestNumbers = numbers => {
+  numbers.sort((a,b) => a - b)
+  return numbers[0] + numbers[1]
+ //return sum
+}
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
