1648e1e9dbb8fd341378aaae2d2335ac1393de8c
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-01-04 14:23:29 -0700

    Update 01-04-24.js

--- 01-04-24.js
+++ 01-04-24.js
@@ -1,19 +1,18 @@
 console.log('Jan 4, 2024')
 
-// kata 1 array
+// kata 1 
 const findNeedle = haystack => 'found the needle at position ' + haystack.indexOf('needle');
-
 console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));
 console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "randomJunk"]));
 
 // kata 2 
-const positiveSum = arr => arr.filter(num => num > 0).reduce((ttl, cv) => ttl + cv, 0)
-console.log(positiveSum([1,-4,7,12]))
+const positiveSum = arr => arr.filter(num => num > 0).reduce((ttl, cv) => ttl + cv, 0);
+console.log(positiveSum([1,-4,7,12]));
 
-// kata 3 //3453455
+// kata 3 
 const sumTwoSmallestNumbers = numbers => {
   numbers.sort((a,b) => a - b)
   return numbers[0] + numbers[1]
  //return sum
 }
-console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]));
