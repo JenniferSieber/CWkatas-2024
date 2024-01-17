86cb69797a5104f07f639a1a94cf45b3ae02b6d1
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-01-17 09:55:44 -0700

    Update jan-17-24.js

--- jan-17-24.js
+++ jan-17-24.js
@@ -6,6 +6,7 @@ const mergeArrays = (a,b) => {
 console.log(mergeArrays([1, 3, 5], [2, 4, 6])) // [1, 2, 3, 4, 5, 6])
 console.log(mergeArrays([2, 4, 8], [2, 4, 6])) //  [2, 4, 6, 8]
 
+
 // kata 2 6kyu
 const getLengthOfMissingArray = arrays => {
   let arr = (arrays || [])
@@ -25,23 +26,10 @@ console.log(getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]
 console.log(getLengthOfMissingArray([[], [1,2]]))
 console.log(getLengthOfMissingArray([[ [ null ], [ null, null, null ]]]))
 
+
 // kata 3 6kyu
 //return "[{name : 'red', id : '1'}, {name : 'yellow', id : '2'}, {name : 'black', id : '3'}, {name : 'white', id : '4'}]
 // string "red 1 yellow 2 black 3 white 4"
-// https://www.codewars.com/kata/5877786688976801ad000100/train/javascript
-
-// const wordsToObject = input=> {
-//   let arr = input.split(' ')
-//   let newArr = []
- 
-//   for (let i = 0; i < arr.length; i++) {
-    
-//     if (i % 2 == 0) {
-//       newArr.push({name: arr[i], id: arr[i + 1]})
-//     }
-//   }
-//   return newArr
-// }
 const wordsToObject = input=> {
   let arr = input.split(' ')
   let newArr = []
